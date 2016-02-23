a52a74f1ac9311edf02330545909d5d9280d0eda
Chris Laplante <mostthingsweb@gmail.com>
2016-02-23 17:55:51 -0500

    wip

--- setup.sh
+++ setup.sh
@@ -4,9 +4,13 @@ function ask_rm() {
     TARGET=$1
 
     [ -d $TARGET ] && {
+        echo "Directory $TARGET exists..."
+        printf "  "
         rm -frI $TARGET
     } || {
         [ -f $TARGET ] && {
+            echo "File $TARGET exists..."
+            printf "  "
             rm -fi $TARGET
         }
     }
@@ -20,5 +24,7 @@ function ask_rm() {
 ask_rm ~/.vimrm
 ask_rm ~/.vim
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://bitbucket.org/MostThingsWeb/dot-vim.git ~/.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim +PluginInstall +qall
